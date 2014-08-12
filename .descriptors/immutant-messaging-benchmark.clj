9eac03b0a8f7f759db4138750a5c13aaeffa424c
josh rotenberg <joshrotenberg@gmail.com>
2014-08-12 11:13:32 -0700

    remove .descriptors

--- .descriptors/immutant-messaging-benchmark.clj
+++
@@ -1,1 +0,0 @@
-{:root "/Users/jrotenberg/Dropbox/immutant-messaging-benchmark", :context-path "immutant-messaging-benchmark", :lein-profiles [:dev :test], :swank-port nil, :nrepl-port 0, :nrepl-port-file "target/test-repl-port"}
